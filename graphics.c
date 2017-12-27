67bccad0f62d7e84bacc0b8fdca2e31d116631f1
root670 <root670@gmail.com>
2017-12-26 18:06:02 -0800

    Correct background graphic dimensions.

--- graphics.c
+++ graphics.c
@@ -576,8 +576,8 @@ void graphicsDrawBackgroundBottom(unsigned int rows)
                                         bg.Height - rows,             // Y1
                                         0,                            // U1
                                         bg.Height - rows,             // V1
-                                        bg.Width,                     // X2
-                                        bg.Height,                    // Y2
+                                        graphicsGetDisplayWidth(),      // X2
+                                        graphicsGetDisplayHeight(),   // Y2
                                         bg.Width,                     // U2
                                         bg.Height,                    // V2
                                         1,                            // Z
