8b0dee9fd509b3d5896dd840e4a8acdfc80bc259
root670 <root670@gmail.com>
2019-04-07 09:07:56 -0700

    Cleanup settings.c. Following a "single success path" paradigm that reduces nesting after checking failure conditions.

--- src/settings.c
+++ src/settings.c
@@ -8,17 +8,24 @@
 #include "graphics.h"
 #include "cheats.h"
 
-typedef struct
+typedef struct settings
 {
     char *databasePath;
     char *bootPaths[5];
 } settings_t;
 
 static int initialized = 0;
-static struct ini_info *ini;
-settings_t settings;
+static settings_t settings;
 static char *diskBootStr = "==Disc==";
 static int settingsChanged = 0;
+
+#ifdef _DTL_T10000
+static char *settingsPath = "host:CheatDevicePS2.ini";
+#else
+static char *settingsPath = "CheatDevicePS2.ini";
+#endif
+
+static char *defaultDatabase = "CheatDevicePS2.cdb";
 static char *defaultBootPaths[] = {
     "mass:BOOT/BOOT.ELF",
     "mass:BOOT/ESR.ELF",
@@ -32,166 +39,122 @@ static const char *HELP_TICKER = \
     "{SQUARE} Options     "
     "{CIRCLE} Main Menu";
 
+static void getINIString(struct ini_info *ini, char **dst, const char *keyName, const char *defaultValue)
+{
+    if(!dst || !keyName || !defaultValue)
+        return;
+
+    const char *value = (ini) ? ini_get(ini, "CheatDevicePS2", keyName) : NULL;
+    if(value)
+        *dst = strdup(value);
+    else
+        *dst = strdup(defaultValue);
+
+    printf("getINIString: %s = %s\n", keyName, *dst);
+}
+
 int initSettings()
 {
-    if(!initialized)
-    {
-        printf("\n ** Initializing Settings **\n");
+    if(initialized)
+        return 0;
 
-        #ifdef _DTL_T10000
-        ini = ini_load("host:CheatDevicePS2.ini");
-        #else
-        ini = ini_load("CheatDevicePS2.ini");
-        #endif
-        if(!ini)
-        {
-            printf("Error opening CheatDevicePS2.ini\n");
-            // Use fallback values
-            settings.databasePath = strdup("CheatDatabase.cdb");
-            settings.bootPaths[0] = strdup(defaultBootPaths[0]);
-            settings.bootPaths[1] = strdup(defaultBootPaths[1]);
-            settings.bootPaths[2] = strdup(defaultBootPaths[2]);
-            settings.bootPaths[3] = strdup(defaultBootPaths[3]);
-            settings.bootPaths[4] = strdup(defaultBootPaths[4]);
-            
-            initialized = 1;
-            return 1;
-        }
-        
-        if(ini_get(ini, "CheatDevicePS2", "database"))
-            settings.databasePath = strdup(ini_get(ini, "CheatDevicePS2", "database"));
-        else
-            settings.databasePath = strdup("CheatDevicePS2.cdb");
-            
-        if(ini_get(ini, "CheatDevicePS2", "boot1"))
-            settings.bootPaths[0] = strdup(ini_get(ini, "CheatDevicePS2", "boot1"));
-        else
-            settings.bootPaths[0] = strdup(defaultBootPaths[0]);
-            
-        if(ini_get(ini, "CheatDevicePS2", "boot2"))
-            settings.bootPaths[1] = strdup(ini_get(ini, "CheatDevicePS2", "boot2"));
-        else
-            settings.bootPaths[1] = strdup(defaultBootPaths[1]);
-            
-        if(ini_get(ini, "CheatDevicePS2", "boot3"))
-            settings.bootPaths[2] = strdup(ini_get(ini, "CheatDevicePS2", "boot3"));
-        else
-            settings.bootPaths[2] = strdup(defaultBootPaths[2]);
-        
-        if(ini_get(ini, "CheatDevicePS2", "boot4"))
-            settings.bootPaths[3] = strdup(ini_get(ini, "CheatDevicePS2", "boot4"));
-        else
-            settings.bootPaths[3] = strdup(defaultBootPaths[3]);
-        
-        if(ini_get(ini, "CheatDevicePS2", "boot5"))
-            settings.bootPaths[4] = strdup(ini_get(ini, "CheatDevicePS2", "boot5"));
-        else
-            settings.bootPaths[4] = strdup(defaultBootPaths[4]);
-        
-        printf("Database path: %s\n", settings.databasePath);
-        printf("Boot path 1: %s\n", settings.bootPaths[0]);
-        printf("Boot path 2: %s\n", settings.bootPaths[1]);
-        printf("Boot path 3: %s\n", settings.bootPaths[2]);
-        printf("Boot path 4: %s\n", settings.bootPaths[3]);
-        printf("Boot path 5: %s\n", settings.bootPaths[4]);
-        
+    printf("\n ** Initializing Settings **\n");
+
+    struct ini_info *ini = ini_load(settingsPath);
+
+    getINIString(ini, &settings.databasePath, "database", defaultDatabase);
+    getINIString(ini, &settings.bootPaths[0], "boot1", defaultBootPaths[0]);
+    getINIString(ini, &settings.bootPaths[1], "boot2", defaultBootPaths[1]);
+    getINIString(ini, &settings.bootPaths[2], "boot3", defaultBootPaths[2]);
+    getINIString(ini, &settings.bootPaths[3], "boot4", defaultBootPaths[3]);
+    getINIString(ini, &settings.bootPaths[4], "boot5", defaultBootPaths[4]);
+
+    if(ini)
         ini_free(ini);
-        initialized = 1;
-        return 1;
-    }
 
-    return 0;
+    initialized = 1;
+    return 1;
 }
 
 int killSettings()
 {
-    if(initialized)
-    {
-        printf(" ** Killing Settings Manager **\n");
-        free(settings.databasePath);
-        free(settings.bootPaths[0]);
-        free(settings.bootPaths[1]);
-        free(settings.bootPaths[2]);
-        free(settings.bootPaths[3]);
-        free(settings.bootPaths[4]);
-        return 1;
-    }
+    if(!initialized)
+        return 0;
 
-    return 0;
+    printf(" ** Killing Settings Manager **\n");
+
+    free(settings.databasePath);
+    free(settings.bootPaths[0]);
+    free(settings.bootPaths[1]);
+    free(settings.bootPaths[2]);
+    free(settings.bootPaths[3]);
+    free(settings.bootPaths[4]);
+
+    return 1;
 }
 
 int settingsSave()
 {
-    if(initialized && settingsChanged)
+    if(!initialized || !settingsChanges)
+        return 0;
+
+    FILE *iniFile = fopen(settingsPath, "w");
+    if(iniFile < 0)
     {
-        FILE *iniFile;
-
-        #ifdef _DTL_T10000
-        iniFile = fopen("host:CheatDevicePS2.ini", "w");
-        #else
-        iniFile = fopen("CheatDevicePS2.ini", "w");
-        #endif
-
-        if(iniFile < 0)
-        {
-            printf("Error saving CheatDevicePS2.ini\n");
-            return 0;
-        }
-
-        fputs("[CheatDevicePS2]\n", iniFile);
-        fprintf(iniFile, "database = %s\n", settings.databasePath);
-        fprintf(iniFile, "boot1 = %s\n", settings.bootPaths[0]);
-        fprintf(iniFile, "boot2 = %s\n", settings.bootPaths[1]);
-        fprintf(iniFile, "boot3 = %s\n", settings.bootPaths[2]);
-        fprintf(iniFile, "boot4 = %s\n", settings.bootPaths[3]);
-        fprintf(iniFile, "boot5 = %s\n", settings.bootPaths[4]);
-
-        fclose(iniFile);
-
-        return 1;
+        printf("Error saving %s\n", settingsPath);
+        return 0;
     }
 
-    return 0;
+    fputs("[CheatDevicePS2]\n", iniFile);
+    fprintf(iniFile, "database = %s\n", settings.databasePath);
+    fprintf(iniFile, "boot1 = %s\n", settings.bootPaths[0]);
+    fprintf(iniFile, "boot2 = %s\n", settings.bootPaths[1]);
+    fprintf(iniFile, "boot3 = %s\n", settings.bootPaths[2]);
+    fprintf(iniFile, "boot4 = %s\n", settings.bootPaths[3]);
+    fprintf(iniFile, "boot5 = %s\n", settings.bootPaths[4]);
+
+    fclose(iniFile);
+
+    return 1;
 }
 
 char* settingsGetDatabasePath()
 {
-    if(initialized)
-    {
-        return settings.databasePath;
-    }
+    if(!initialized)
+        return NULL;
 
-    return NULL;
+    return settings.databasePath;
 }
 
 void settingsSetDatabasePath(const char *path)
 {
-    if(path)
-    {
-        free(settings.databasePath);
-        settings.databasePath = strdup(path);
-        settingsChanged = 1;
-    }
+    if(!initialized || !path)
+        return;
+
+    free(settings.databasePath);
+    settings.databasePath = strdup(path);
+    settingsChanged = 1;
 }
 
 char** settingsGetBootPaths(int *numPaths)
 {
-    if(initialized)
-    {
-        *numPaths = 5;
-        return settings.bootPaths;
-    }
-    return NULL;
+    if(!initialized)
+        return NULL;
+
+    *numPaths = 5;
+    return settings.bootPaths;
 }
 
 void settingsLoadBootMenu()
 {
-    char **paths;
+    if(!initialized)
+        return;
+
     int numPaths;
-    
-    paths = settingsGetBootPaths(&numPaths);
+    char **paths = settingsGetBootPaths(&numPaths);
     
     menuItem_t *items = calloc(numPaths + 1, sizeof(menuItem_t));
+    
     /* Disc boot; default option */
     items[0].type = MENU_ITEM_NORMAL;
     items[0].text = diskBootStr;
@@ -212,25 +175,24 @@ void settingsLoadBootMenu()
 
 void settingsRenameBootPath()
 {
+    if(!initialized)
+        return;
+
+    if(menuGetActive() != MENU_BOOT)
+        return;
+
+    char **bootPath = (char **)menuGetActiveItemExtra();
+    if(!bootPath)
+        return;
+
     char newPath[80];
+    if(displayTextEditMenu(newPath, sizeof(newPath), menuGetActiveItemText(), "Edit Boot Path") == 0)
+        return;
 
-    if(initialized)
-    {
-        if(menuGetActive() != MENU_BOOT)
-            return;
-
-        char **bootPath = (char **)menuGetActiveItemExtra();
-        if(!bootPath)
-            return;
-        
-        if(displayTextEditMenu(newPath, sizeof(newPath), menuGetActiveItemText(), "Edit Boot Path") == 0)
-            return;
-
-        free(*bootPath);
-        *bootPath = strdup(newPath);
-        menuRenameActiveItem(newPath);
-        menuRemoveAllItems();
-        settingsLoadBootMenu();
-        settingsChanged = 1;
-    }
+    free(*bootPath);
+    *bootPath = strdup(newPath);
+    menuRenameActiveItem(newPath);
+    menuRemoveAllItems();
+    settingsLoadBootMenu();
+    settingsChanged = 1;
 }
