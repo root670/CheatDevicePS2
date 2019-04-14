eb7d469b05128afd35a5ae17e6d46c78f36c070d
root670 <root670@gmail.com>
2019-04-14 15:33:22 -0700

    Use const pointer for hashFunction().

--- src/hash.h
+++ src/hash.h
@@ -15,11 +15,12 @@
 hashTable_t *hashNewTable(int numEntries);
 // Free hash table
 void hashDestroyTable(hashTable_t *table);
-// Get pointer to data associated with hash
+// Get pointer to data associated with hash. Returns NULL if the hash doesn't
+// exist in the table.
 void *hashFind(hashTable_t *table, unsigned int hash);
 // Add hash-pointer pair to table
 void hashAdd(hashTable_t *table, void *ptr, unsigned int hash);
 // Get hash value for key
-unsigned int hashFunction(void *key, int len);
+unsigned int hashFunction(const void *key, int len);
 
 #endif
